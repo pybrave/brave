f081e36d46c6b698f3e43bac8d5bbeb1614e0cb1
wangyang <1749748955@qq.com>
2025-06-26 18:25:37 +0800

    support pipleine online edit

---
+++ brave/api/schemas/pipeline.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class SavePipeline(BaseModel):
+    pipeline_id: Optional[str]=None
+    parent_pipeline_id:Optional[str]=None
+    pipeline_key: Optional[str]=None
+    pipeline_order: Optional[int]=None
+    pipeline_type: Optional[str]
+    content: Optional[str]=None
+
+class QueryPipeline(BaseModel):
+    pipeline_id: Optional[str]=None
+    parent_pipeline_id:Optional[str]=None
+    pipeline_key: Optional[str]=None
+    pipeline_order: Optional[int]=None
+    pipeline_type: Optional[str]=None
+    content: Optional[str]=None
+
+class Pipeline(BaseModel):
+    id:Optional[int]
+    pipeline_id: Optional[str]
+    parent_pipeline_id:Optional[str]
+    pipeline_key: Optional[str]
+    pipeline_order: Optional[int]
+    pipeline_type: Optional[str]
+    content: Optional[str]
--- brave/api/service/pipeline.py
+++ brave/api/service/pipeline.py
@@ -3,6 +3,7 @@ import os
 import glob
 from brave.api.config.config import get_settings
 from pathlib import Path
+import shutil
 
 
 def get_pipeline_dir():
@@ -25,3 +26,39 @@ def get_all_module(type):
     nextflow_list = glob.glob(f"{pipeline_dir}/*/{type}/*.py")
     nextflow_dict = {os.path.basename(item).replace(".py",""):get_module_name(item) for item in nextflow_list}
     return nextflow_dict
+
+def delete_wrap_pipeline_dir(pipeline_key):
+    pipeline_dir =  get_pipeline_dir()
+    src  =f"{pipeline_dir}/{pipeline_key}"
+    dst  =f"{pipeline_dir}/bin"
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    if os.path.exists(src):
+        shutil.move(src,dst)
+
+def create_wrap_pipeline_dir(pipeline_key):
+    pipeline_dir = get_pipeline_dir()
+    img = f"{pipeline_dir}/{pipeline_key}/img"
+    nextflow = f"{pipeline_dir}/{pipeline_key}/nextflow"
+    py_parse_analysis = f"{pipeline_dir}/{pipeline_key}/py_parse_analysis"
+    py_parse_analysis_result = f"{pipeline_dir}/{pipeline_key}/py_parse_analysis_result"
+    py_plot = f"{pipeline_dir}/{pipeline_key}/py_plot"
+    dir_list = [img, nextflow,py_parse_analysis,py_parse_analysis_result,py_plot]
+    for item in dir_list:
+        if not os.path.exists(item):
+            os.makedirs(item) 
+
+def create_file(pipeline_key,pipeline_type,content):
+    pipeline_dir = get_pipeline_dir()
+    pipeline_dir = f"{pipeline_dir}/{pipeline_key}"
+    if pipeline_type == "wrap_pipeline":
+        analysisPipline = f"{pipeline_dir}/nextflow/{content['analysisPipline']}.nf"
+        parseAnalysisModule = f"{pipeline_dir}/py_parse_analysis/{content['parseAnalysisModule']}.py"
+        if not os.path.exists(analysisPipline):
+            with open(analysisPipline,"w") as f:
+                f.write("")
+        if not os.path.exists(parseAnalysisModule):
+            with open(parseAnalysisModule,"w") as f:
+                f.write("")
+
+    
