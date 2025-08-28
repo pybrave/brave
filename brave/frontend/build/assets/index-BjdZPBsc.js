b0d58fab427d9bd55d42b2a26da970198117bcab
wangyang <1749748955@qq.com>
2025-08-28 18:11:37 +0800

    update ui

--- brave/frontend/build/assets/index-BjdZPBsc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4nLTTepg.js";const i=()=>r.jsx("div",{children:"Script"});export{i as default};
---
+++ brave/frontend/build/assets/index-Cj__iivH.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D_hCWDFH.js";const i=()=>r.jsx("div",{children:"Script"});export{i as default};
