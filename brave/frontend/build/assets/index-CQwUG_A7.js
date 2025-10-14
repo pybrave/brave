d05f812f9196fc0ba9190cf3ad6a985a2a7f7d57
wangyang <1749748955@qq.com>
2025-10-14 16:47:04 +0800

    update ui

--- brave/frontend/build/assets/index-CQwUG_A7.js
+++
@@ -1,5 +0,0 @@
-import{r as i,j as t}from"./main-DXb8rb93.js";import o from"./index-KXj2g4Cn.js";import"./index-D122PzCT.js";import"./addEventListener-BOvmtz4c.js";const r=`
-# Bioinformatics Reactive Analysis and Visualization Engine(BRAVE)
-
-![](/brave-api/img/logo.png)
-`,c=()=>{const[a,s]=i.useState(r);return t.jsx("div",{style:{maxWidth:"1000px",margin:"1rem auto"},children:t.jsx(o,{data:a})})};export{c as default};
--- brave/frontend/build/assets/index-k5EWVEYG.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./main-DXb8rb93.js";const i=()=>r.jsx("div",{children:"Script"});export{i as default};
